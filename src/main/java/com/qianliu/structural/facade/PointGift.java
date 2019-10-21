3097fec840f49bf1bb22c8fc3655fd95193af825
liu <qianliu@zhuoyuan-info.com>
2019-10-21 17:56:06 +0800

    feat: 新增springboot整和mybatis-plus

---
+++ src/main/java/com/qianliu/structural/facade/PointGift.java
@@ -0,0 +1,16 @@
+package com.qianliu.structural.facade;
+
+/**
+ * 积分礼物
+ */
+public class PointGift {
+    private String name;
+
+    public PointGift(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
