876cb4707db2409201e95281f58598ba37d4550d
liu <qianliu@zhuoyuan-info.com>
2019-10-21 18:01:35 +0800

    Revert "Revert "feat: 新增springboot整和mybatis-plus""
    This reverts commit 7db1b3463bfe4fdca192b8d38379b2f66738e26c.

---
+++ src/main/java/com/qianliu/structural/decorator/v1/Battercake.java
@@ -0,0 +1,12 @@
+package com.qianliu.structural.decorator.v1;
+
+
+public class Battercake {
+    protected String getDesc(){
+        return "煎饼";
+    }
+    protected int cost(){
+        return 8;
+    }
+
+}
